12e77d058a8322959465a11175c6ecbd53bc9833
drnoxii <jdconislla@gmail.com>
2026-05-31 02:01:17 -0500

    creacion de Interface y sus metodos

---
+++ src/java/Interface/IPedido.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interface;
+
+import Model.Pedido;
+
+/**
+ *
+ * @author spide
+ */
+public interface IPedido {
+    public int generarPedido(Pedido pedidos);
+}
